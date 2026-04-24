02725974111d2e9b16e7d626d5ff704e0698e3ff
icyriverbreeze-vt <icyriverbreeze@gmail.com>
2026-04-24 16:47:05 -0500

    Update tcpser4j.bat

--- tcpser4j.bat
+++ tcpser4j.bat
@@ -1,2 +1,2 @@
 set PATH=.;lib;%PATH%
-java -cp lib\tcpser4j-2.0rc6.jar;lib\dom4j-full.jar;lib\RXTXcomm.jar;lib\log4j-1.2.5.jar;. org.jbrain.tcpser4j.TCPSerial config.xml
+java -cp lib\tcpser4j-2.0rc6.jar;lib\dom4j-2.2.0.jar;lib\RXTXcomm.jar;lib\log4j-1.2.5.jar;. org.jbrain.tcpser4j.TCPSerial config.xml
